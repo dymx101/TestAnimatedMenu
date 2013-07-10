4c30e7c82b9a102aa733ee90552bb61e659d95bc
dymx101 <dongyiming@Dongs-MacBook-Pro.local>
2013-07-09 19:16:20 -0700

    tuning

--- TestAnimatedMenu/GGAnimatedMenu.m
+++ TestAnimatedMenu/GGAnimatedMenu.m
@@ -9,10 +9,13 @@
 #import "GGAnimatedMenu.h"
 #import <QuartzCore/QuartzCore.h>
 
+#define DEFAULT_BUTTON_SIZE         40
+#define DEFAULT_CIRCLE_RADIOUS      100
 
 @implementation GGAnimatedMenu
 {
     NSMutableArray      *_menuItems;
+    UIView              *_dimedView;
 }
 
 -(id)initWithCoder:(NSCoder *)aDecoder
@@ -38,20 +41,43 @@
 -(void)_doInit
 {
     _menuItems = [NSMutableArray array];
+    
+    _dimedView = [[UIView alloc] initWithFrame:self.bounds];
+    _dimedView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    _dimedView.backgroundColor = [UIColor orangeColor];
+    _dimedView.layer.opacity = .5f;
+    [self addSubview:_dimedView];
 }
 
 -(void)showInView:(UIView *)aView
 {
+    self.frame = aView.bounds;
+    int itemCount = _menuItems.count;
     
+    if (itemCount)
+    {
+        float startAngel = M_PI_2;
+        float angelStep = 2 * M_PI / itemCount;
+        CGPoint centerPt = self.center;
+        
+        for (int i = 0; i < itemCount; i++)
+        {
+            
+        }
+    }
 }
 
 -(void)addItemWithImage:(UIImage *)aImage selectedImage:(UIImage *)aSelectedImage target:(id)aTarget action:(SEL)anAction
 {
     UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    btn.frame = CGRectMake(0, 0, DEFAULT_BUTTON_SIZE, DEFAULT_BUTTON_SIZE);
     btn.backgroundColor = [UIColor blackColor];
     btn.layer.opacity = .9f;
     [btn setImage:aImage forState:UIControlStateNormal];
     [btn setImage:aSelectedImage forState:UIControlStateSelected | UIControlStateHighlighted];
+    
+    [self addSubview:btn];
+    [_menuItems addObject:btn];
 }
 
 @end
--- TestAnimatedMenu/GGViewController.m
+++ TestAnimatedMenu/GGViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "GGViewController.h"
+#import "GGAnimatedMenu.h"
 
 @interface GGViewController ()
 
@@ -17,13 +18,11 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	
+    GGAnimatedMenu *menu = [[GGAnimatedMenu alloc] initWithFrame:self.view.bounds];
+    [self.view addSubview:menu];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
+
 
 @end
